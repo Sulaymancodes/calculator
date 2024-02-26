a0742dccae163e5c95be7182614981a5544ce287
sulaiman-rabiu <sulaymancodes@gmail.com>
2024-02-26 22:42:14 +0100

    Created Functions For The Calculator Operation
    Function is created for the 4(+,-,*,/) operations
    and they were linked to the UI to perform the
    operations.

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const calculatorScreen = document.querySelector('.cal-screen');
+const operatorBtns = document.querySelectorAll('.operator-btn');
 const equalBtn = document.querySelector('.equal-btn');
 const buttons = document.querySelectorAll('.btns');
+const clearBtn = document.querySelector('.ac-del');
+const deleteBtn = document.querySelector('.del');
+
 let firstValue = 0;
 let secondValue = 0;
-let operator = ''
+let operator = 0;
 
 function add(firstValue,secondValue){
-    return firstValue + secondValue;
+    return calculatorScreen.textContent= firstValue + secondValue;
 }
 function subtract(firstValue,secondValue){
-    return firstValue - secondValue;
+    return calculatorScreen.textContent= firstValue - secondValue;
 }
 function multiply(firstValue,secondValue){
-    return firstValue * secondValue;
+    return calculatorScreen.textContent= firstValue * secondValue;
 }
 function divide(firstValue,secondValue){
-    return firstValue / secondValue;
+    return calculatorScreen.textContent= firstValue / secondValue;
 }
 
 function operate(firstValue,secondValue,operator){
@@ -25,10 +29,10 @@ function operate(firstValue,secondValue,operator){
     else if(operator === '-'){
         subtract(firstValue,secondValue)
     }
-    else if(operator === '*'){
+    else if(operator === '×'){
         multiply(firstValue,secondValue)
     }
-    else if(operator === '/'){
+    else if(operator === '÷'){
         divide(firstValue,secondValue)
     }
 }
@@ -37,6 +41,51 @@ function operate(firstValue,secondValue,operator){
 buttons.forEach(function(button){
     button.addEventListener('click', function(){
         let btnValue = button.value;
-        calculatorScreen.textContent += btnValue;
+        if(firstValue !== 0 && secondValue !== 0){
+            calculatorScreen.textContent = '';
+            firstValue = 0;
+            secondValue = 0;
+        }
+        if (btnValue === '.' && calculatorScreen.textContent.includes('.')) {
+            return; // Exit the function early if "." is already present
+        }
+        if(calculatorScreen.textContent === '0'){
+            calculatorScreen.textContent = btnValue
+        }
+        else{
+            calculatorScreen.textContent += btnValue;
+        }
     })
+})
+
+operatorBtns.forEach(function(opeBtn){
+    opeBtn.addEventListener('click', () =>{
+        operator = opeBtn.value;
+        firstValue = Number(calculatorScreen.textContent)
+        calculatorScreen.textContent = '';
+    })
+})
+
+equalBtn.addEventListener('click', () =>{
+    secondValue = Number(calculatorScreen.textContent);
+    operate(firstValue,secondValue,operator); 
+    
+});
+
+clearBtn.addEventListener('click',() => {
+    firstValue = 0;
+    secondValue = 0;
+    calculatorScreen.textContent = '0';
+})
+
+deleteBtn.addEventListener('click',() =>{
+    let calValue = calculatorScreen.textContent;
+    if(calValue === '0'){
+        calValue = '0'
+    }
+    else{
+        let newCalValue = calValue.slice(0,-1)
+        calculatorScreen.textContent = newCalValue;
+    }
+   
 })
